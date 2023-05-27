07dcb6523762c0ba5718b88c3a9c53f18f3baa9f
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2023-05-26 19:37:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def connection():
     
-    cnxn = mysql.connector.connect(user="thesquashedman", password="Mesoepic2", host="pavelserver.mysql.database.azure.com", port=3306, database="library")
+    cnxn = mysql.connector.connect(user="thesquashedman", password="Mesoepic2", host="pavelserver.mysql.database.azure.com", port=3306, database="library", ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
     cursor = cnxn.cursor()
     cursor.execute("SELECT * FROM test;")
     
